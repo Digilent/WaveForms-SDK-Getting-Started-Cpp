c01f238d6a70e7b28974a02b20eb2e70c2365976
Álmos Veres-Vitályos <almos.veres-vitalyos@digilent.ro>
2021-12-09 14:34:35 +0200

    restructuring base classes to include function and trigger types as static classes

--- WF_SDK/wavegen.h
+++ WF_SDK/wavegen.h
@@ -13,25 +13,24 @@
 
 /* ----------------------------------------------------- */
 
-class Function {
-    /* function names */
-    FUNC custom = funcCustom;
-    FUNC sine = funcSine;
-    FUNC square = funcSquare;
-    FUNC triangle = funcTriangle;
-    FUNC noise = funcNoise;
-    FUNC dc = funcDC;
-    FUNC pulse = funcPulse;
-    FUNC trapezium = funcTrapezium;
-    FUNC sine_power = funcSinePower;
-    FUNC ramp_up = funcRampUp;
-    FUNC ramp_down = funcRampDown;
-} function;
-
-/* ----------------------------------------------------- */
-
 class Wavegen {
     public:
+        class function {
+            /* function names */
+            public:
+                static const FUNC custom = funcCustom;
+                static const FUNC sine = funcSine;
+                static const FUNC square = funcSquare;
+                static const FUNC triangle = funcTriangle;
+                static const FUNC noise = funcNoise;
+                static const FUNC dc = funcDC;
+                static const FUNC pulse = funcPulse;
+                static const FUNC trapezium = funcTrapezium;
+                static const FUNC sine_power = funcSinePower;
+                static const FUNC ramp_up = funcRampUp;
+                static const FUNC ramp_down = funcRampDown;
+        };
+
         void generate(HDWF device_handle, int channel, FUNC function, double offset, double frequency = 1e03, double amplitude = 1, double symmetry = 50, double wait = 0, double run_time = 0, int repeat = 0, double* data = nullptr);
         void close(HDWF device_handle);
 } wavegen;
