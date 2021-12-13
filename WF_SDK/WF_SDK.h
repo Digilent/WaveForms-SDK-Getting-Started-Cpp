aa1cdb3d9f851a73f241be0192b92217b364a435
Álmos Veres-Vitályos <almos.veres-vitalyos@digilent.ro>
2021-12-13 16:04:29 +0200

    correct includes

--- WF_SDK/WF_SDK.h
+++ WF_SDK/WF_SDK.h
@@ -2,18 +2,6 @@
     This module realizes communication with Digilent Test & Measurement devices
 */
 
-#include "device.h"
-#include "scope.h"
-#include "wavegen.h"
-#include "supplies.h"
-#include "dmm.h"
-#include "logic.h"
-#include "pattern.h"
-#include "static.h"
-#include "protocol/uart.h"
-#include "protocol/spi.h"
-#include "protocol/i2c.h"
-
 #include "device.cpp"
 #include "scope.cpp"
 #include "wavegen.cpp"
