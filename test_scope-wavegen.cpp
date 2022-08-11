d2b4a06f4b9bf3f21f33943ee68f2e474c4c498f
Almos Veres-Vitalyos <almos.veres-vitalyos@ni.com>
2022-08-11 14:15:55 +0300

    display spectrum in example

--- test_scope-wavegen.cpp
+++ test_scope-wavegen.cpp
@@ -25,6 +25,7 @@ int main(void) {
 
         // generate a 10KHz sine signal with 2V amplitude on channel 1
         wavegen.generate(device_data, 1, wavegen.function.sine, 0, 10e03, 2);
+        tools.sleep(1000);
 
         // record data with the scope on channel 1
         std::vector<double> buffer = scope.record(device_data, 1);
@@ -34,18 +35,36 @@ int main(void) {
         if (length > 10000) {
             length = 10000;
         }
+        buffer.resize(length);
         
-        // save data
+        // save time domain data
         std::ofstream file;
         file.open("test_scope-wavegen.csv");
         file << "time [ms],voltage [V]\n";
-        for (int index = 0; index < length; index++) {
+        for (int index = 0; index < buffer.size(); index++) {
             file << std::to_string(index * 1e03 / scope.data.sampling_frequency) << "," << std::to_string(buffer[index]) << std::endl;
         }
         file.close();
 
         // plot
         system("python plotting.py test_scope-wavegen.csv");
+
+        // compute the spectrum from 0Hz to 100KHz
+        double start_frequency = 0;
+        double stop_frequency = 100e03;
+        std::vector<double> spectrum = tools.spectrum(buffer, tools.window.flat_top, scope.data.sampling_frequency, start_frequency, stop_frequency);
+
+        // save frequency domain data
+        file.open("test_scope-wavegen_spectrum.csv");
+        file << "frequency [MHz],magnitude [dBV]\n";
+        double step = (stop_frequency - start_frequency) / (spectrum.size() - 1);
+        for (int index = 0; index < spectrum.size(); index++) {
+            file << std::to_string((start_frequency + index * step) / 1e06) << "," << std::to_string(spectrum[index]) << std::endl;
+        }
+        file.close();
+
+        // plot
+        system("python plotting.py test_scope-wavegen_spectrum.csv");
 
         // reset the scope
         scope.close(device_data);
